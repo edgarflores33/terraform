c5632c6dbaa958f5b0cf6ee784e9f232a871c456
Edgar Flores <eflormal@emeal.nttdata.com>
2024-05-28 11:23:11 -0500

    cuarto commit

--- sus-ciam-dev/provider.tf
+++ sus-ciam-dev/provider.tf
@@ -11,6 +11,8 @@ terraform {
 provider "azurerm" { 
   # tenant_id       = var.TENANT_ID
   # subscription_id = var.SUBSCRIPTION_ID_MAIN
+  tenant_id = "7659b7f1-9b0f-4f0e-987d-5bfd54aa08cd"
+  subscription_id = "e4b49579-e522-4b37-860a-e606f131e4c4"
   features {
 
   }
