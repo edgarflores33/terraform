fa9c6188784f5c504e16b11e05e100c340749f67
Edgar Flores <eflormal@emeal.nttdata.com>
2024-06-03 10:07:25 -0500

    cuarto commit

--- python/function.py
+++ python/function.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from flask_executor import Executor
+from datetime import datetime, timedelta
 import time
 import requests
  
@@ -9,7 +10,7 @@ executor = Executor(app)
 url_create = f'https://api.github.com/repos/edgarflores33/terraform/actions/workflows/create.yaml/dispatches'
 url_delete = f'https://api.github.com/repos/edgarflores33/terraform/actions/workflows/delete.yaml/dispatches'
 headers = {
-    "Authorization": "token ",
+    "Authorization": "token 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",
     'Accept': 'application/vnd.github.v3+json'
 }
  
@@ -17,12 +18,12 @@ params = {
     'ref': 'main',  # Reemplaza con la rama donde quieres ejecutar el workflow
 }
  
-def delete(segundos):
+def delete(segundos:int):
     print(segundos)
     #Wait 5 minutes
     time.sleep(segundos) # 5 minutes
     # time.sleep(8 * 60 * 60) # 8 hours
-    response = requests.post(url_delete, headers=headers,json=params)
+    response = requests.post(url_delete, headers=headers, json=params)
     #Check the response
     if response.status_code == 204:
         print("Event dispatched successfully.")
@@ -32,18 +33,27 @@ def delete(segundos):
 @app.route('/webhook', methods=['POST'])
 def handle_webhook():
     data = request.get_json()
-    status=data["issue"]["fields"]["status"]["name"]
-    if status=='Done':
+    status = data["issue"]["fields"]["status"]["name"]
+    if status == 'Done':
         response = requests.post(url_create, headers=headers, json=params)
         #Check the response
         if response.status_code == 204:
             print("Event dispatched successfully.")
         else:
             print(f"Failed to dispatch event: {response.content}")
-        segundos = 30
-        executor.submit(delete(segundos))
-        return '', 200
-       
+        
+        # Get the current date and time
+        start_date = datetime.now()        
+        # Add 8 hours to the current date and time
+        end_date = datetime.strptime('2024-06-03 10:10', '%Y-%m-%d %H:%M')        
+        # Calculate the difference between the two dates
+        difference = end_date - start_date        
+        # Get the difference in seconds
+        difference_in_seconds :int = difference.total_seconds()        
+        print(f"Difference in seconds: {difference_in_seconds}")
+        # segundos = 30
+        executor.submit(delete(difference_in_seconds))
+        return '', 200       
     else:
         return '', 200
 if __name__ == '__main__':
--- python/hora.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-date_time_str = "28 January 2023 08:20:00"
-
-date_object = datetime.strptime(date_time_str, "%d %B %Y %H:%M:%S").time()
-
-print(date_object)
-
-#print(type(date_object))
